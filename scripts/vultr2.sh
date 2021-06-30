c948297b6d22fbd1114992a2095cdd5546ae5fc9
oanhvo2303 <83030568+oanhvo2303@users.noreply.github.com>
2021-06-30 20:50:14 +0700

    Update vultr2.sh

--- scripts/vultr2.sh
+++ scripts/vultr2.sh
@@ -74,8 +74,8 @@ EOF
 upload_proxy() {
     cd $WORKDIR
     local PASS=123456
-    zip --password $PASS linhpvt.zip proxy.txt
-    URL=$(curl -F "file=@linhpvt.zip" https://file.io)
+    zip --password $PASS proxy.zip proxy.txt
+     URL=$(curl https://bashupload.com/proxy.zip --data-binary proxy.zip)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
